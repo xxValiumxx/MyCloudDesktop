7c9517cc6b64912c4163c764ecc06ed9d59f3531
Valium <takeyourvalium@gmail.com>
2012-11-25 20:17:14 -0800

    Initial Commit, has taskbar and right click handling.

---
+++ js/index.js
@@ -0,0 +1,42 @@
+﻿$(document).ready(function () {
+
+    $("#loginbutton").click(function () {
+        $("#loginbox").animate({
+            opacity: 0,
+        }, 500, function () {
+            $("#loginbox").css('display', 'none');
+            $("#loginbox").remove();
+        });        
+    });
+    //Main Body Right-Click Handler  MUST BE OVERRIDDEN ON NON RIGHT CLICKABLES!
+    
+    $("#desktop").bind("contextmenu", function (e) {
+        e.preventDefault();
+        $("div.context-menu").hide();
+        $("div.context-menu").remove();
+        $("<div class='context-menu'>Desktop Context Menu</div>")
+        .appendTo("body")
+        .css({ top: e.pageY + "px", left: e.pageX + "px" });
+    });
+    
+    //Taskbar Right-Click Handler
+    $("#taskbar").bind("contextmenu", function (e) {
+        e.preventDefault();
+        $("div.context-menu").hide();
+        $("div.context-menu").remove();
+        $("<div class='context-menu'>Taskbar Context Menu</div>")
+        .appendTo("body")
+        .css({ top: e.pageY + "px", left: e.pageX + "px" });
+    });
+    //Handle click to get rid of context menus
+    $(document).bind("click", function (event) {
+        $("div.context-menu").hide();
+        $("div.context-menu").remove();
+    });
+
+
+
+
+
+
+});
